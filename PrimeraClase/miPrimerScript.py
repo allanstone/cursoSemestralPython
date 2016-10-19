495ab27690df68b0e3b0f6bf0cede4342565aacd
Alan Garrido <allanstone19@gmail.com>
2016-10-18 20:05:31 -0500

    Agregando Primera Clase
    Describimos algunas de las propiedades del lenguaje y vimos un pequeño
    ejemplo de como hacer dos scripts simples pero realmente poderosos :)

---
+++ PrimeraClase/miPrimerScript.py
@@ -0,0 +1,20 @@
+#Hola mundo en python
+#recordar que los scripts se ejecutan de la siguente manera
+# windows:
+# C:\python miScript.py
+# linux o mac (se debe indicar la versión de python)
+# $ python3 miScript.py
+print("Hola mundo desde un script!!!")
+
+#Para crear variables se usa el operador de asignación "=" 
+a=0
+b=2
+#Usar operadores aritméticos es muy simple 
+c=a+b
+#Con la función type vemos el tipo que corresponde una variable
+print(type(c))
+#Y la imprimimos en el script
+print(c)
+#imprimir una cadena y un número
+#no conctatenar "cadena"+numero <- ERROR
+print("El número c: ",c)
---
+++ PrimeraClase/nombre.py
@@ -0,0 +1,9 @@
+#este script lee de teclado un nombre
+'''comentario
+de más
+de una linea
+'''
+#para pedir cadenas de texto desde el teclado usamos input()
+nombre=input("Dame tu nombre: ")
+#Para concatenar cadenas usamos el operador de suma
+print("Hola "+nombre)
